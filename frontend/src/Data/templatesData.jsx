4a01673db8690311734806ead7e83562aed788d4
dhruvBvm <dhruvp.bvminfotech@gmail.com>
2026-03-16 12:21:12 +0530

    Updated frontend PDF File

---
+++ frontend/src/Data/templatesData.jsx
@@ -0,0 +1,38 @@
+import TemplateOneImage from "../Assests/Images/templateOneImage.jpg";
+import TemplateTwoImage from "../Assests/Images/templateTwoImage.avif";
+import TemplateThreeImage from "../Assests/Images/templateThreeImage.avif";
+import TemplateFourImage from "../Assests/Images/templateFourImage.avif";
+import TemplateFiveImage from "../Assests/Images/templateFiveImage.avif";
+
+export const templatesdata = [
+  {
+    imagePath: TemplateOneImage,
+    name: "Chandan",
+    id: "201",
+    tagline: "Sleek & Professional",
+  },
+  {
+    imagePath: TemplateTwoImage,
+    name: "Dhruv",
+    id: "202",
+    tagline: "Creative & Bold",
+  },
+  {
+    imagePath: TemplateThreeImage,
+    name: "Sanil",
+    id: "203",
+    tagline: "Minimalist Focus",
+  },
+  {
+    imagePath: TemplateFourImage,
+    name: "Parth",
+    id: "204",
+    tagline: "Modern Dark Mode",
+  },
+  {
+    imagePath: TemplateFiveImage,
+    name: "Jay",
+    id: "205",
+    tagline: "Premium Aesthetic",
+  },
+];
